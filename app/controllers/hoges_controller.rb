ba3e96b5bd935a61f117cc2e804b2c2c9a6fdb31
rchaser53 <tayoshizawa29@gmail.com>
2017-04-05 23:12:01 +0900

    try to make a post button

--- app/controllers/hoges_controller.rb
+++ app/controllers/hoges_controller.rb
@@ -1,5 +1,14 @@
 class HogesController < ApplicationController
   def new
     @hoge = Hoge.new
+    puts 'gao-n'
+  end
+
+  def nyanchu
+    288888
+  end
+
+  def jjj
+    puts 'abc'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'hoges/new'
-
+  post 'hoges/jjj'
   root 'root#poyo'
 end
